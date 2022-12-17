f105f4429737e6b4e80b54ff2d32a73b8534eae5
heyyakash <akashsharma2002@gmail.com>
2022-12-17 22:43:17 +0530

    fixed cors

--- components/Call.js
+++ components/Call.js
@@ -1,8 +1,11 @@
 import React, { useEffect,useRef,useState } from 'react'
+import { useLoader } from '@react-three/fiber'
 import { Mesh } from 'three'
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
 import { useFrame, useThree } from '@react-three/fiber'
 
 const Call = () => {
+    
     const [position,setPosition] = useState([0,-1,0])
     useEffect(()=>{
         window.addEventListener('keypress',(e)=>changePosition(e))
@@ -23,14 +26,21 @@ const Call = () => {
         if(e.key==='s') setPosition([position[0],position[1],position[2]+0.5])
         if(e.key==='a') setPosition([position[0]-0.5,position[1],position[2]])
         if(e.key==='d') setPosition([position[0]+0.5,position[1],position[2]])
-   
+        console.log(mesh.current.position)
     }
-    return (
-        <mesh ref= {mesh} position={position}>
-            <boxGeometry />
-            <meshStandardMaterial />
-        </mesh>
-    )
+    const obj = useLoader(OBJLoader, '/astronaut/Astronaut.obj')
+  return (
+    <>
+    <pointLight color = {'tomato'} />
+    <primitive object={obj} ref = {mesh} position={position}     />
+    </>
+  )
+    // return (
+        // <mesh ref= {mesh} position={position}>
+        //     <boxGeometry />
+        //     <meshStandardMaterial />
+        // </mesh>
+    // )
 }
 
 export default Call
--- pages/room/[slug].js
+++ pages/room/[slug].js
@@ -1,10 +1,20 @@
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useEffect } from 'react'
 import {io} from 'socket.io-client'
 
 const Room = () => {
-    const socket = io('https://aeff-2409-4071-2483-a281-18a1-be01-6ae9-c7e5.in.ngrok.io/')
-    console.log(socket)
+    // const socket = io('https://aeff-2409-4071-2483-a281-18a1-be01-6ae9-c7e5.in.ngrok.io/')
+    useEffect(()=>{
+        const socket = io('https://orange-cougars-suffer-157-50-69-136.loca.lt',{
+            extraHeaders:{
+                'Bypass-Tunnel-Reminder':"Hello"
+            }
+        })
+    socket.emit('joinRoom',{username:"Akash",room:"hello"})
+    socket.on('message',data=>console.log(data))
+    },[])
+    
+    // console.log(socket)
     const router = useRouter()
     const {slug} = router.query
   return (
