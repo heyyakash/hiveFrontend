90fea7fc05fce175d0b5bffc8be97fad084adb58
heyyakash <akashsharma2002@gmail.com>
2022-12-18 07:33:41 +0530

    finished

--- components/Message/Message.js
+++ components/Message/Message.js
@@ -6,7 +6,7 @@ import { io } from 'socket.io-client';
 const ChatWindow = () => {
     return (
         <Html transform occlude position={[0, 40, 0]} className="w-[500px] h-[500px] bg-white/80 p-4 rounded-xl overflow-auto flex flex-col">
-            <iframe src="https://0e7b-103-169-236-82.in.ngrok.io/" className='w-full h-full' frameborder="0"></iframe>
+            <iframe src="https://5204-103-169-236-82.in.ngrok.io" className='w-full h-full' frameborder="0"></iframe>
 
         </Html>
     );
--- components/VideoCall.js
+++ components/VideoCall.js
@@ -4,8 +4,7 @@ import React from 'react'
 const VideoCall = () => {
   return (
     <Html transform occlude position={[40,20,20]} className = "w-[500px] h-[500px] bg-white/80 rounded-xl " >
-        <iframe src="https://701b-103-169-236-82.in.ngrok.io/" allow="camera;microphone" className='w-full h-full' frameborder="0"></iframe>
-
+        <iframe src="https://3635-103-169-236-82.in.ngrok.io/" allow="camera;microphone" className='w-full h-full' frameborder="0"></iframe>
     </Html>
   )
 }
